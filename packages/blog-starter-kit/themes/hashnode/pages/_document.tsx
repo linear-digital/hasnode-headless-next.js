f8b6d3a93fba423079919af031d8414cd306f1a7
linear-digital <tamizuddin.tr@gmail.com>
2025-02-19 23:22:18 +0600

    update _document.tsx

--- packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html lang="en">
+		<Html amp lang="en">
 			<Head>
 				{/* Google AdSense Meta Tag */}
 				<meta name="google-adsense-account" content="ca-pub-4236044300590126" />
@@ -13,8 +13,18 @@ export default function Document() {
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4236044300590126"
 					crossOrigin="anonymous"
 				></script>
+
+				{/* AMP Auto Ads Script */}
+				<script 
+					async 
+					custom-element="amp-auto-ads"
+					src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
+				></script>
 			</Head>
 			<body>
+				{/* AMP Auto Ads */}
+				<amp-auto-ads type="adsense" data-ad-client="ca-pub-4236044300590126"></amp-auto-ads>
+
 				<Main />
 				<NextScript />
 				<div id="hn-toast" />
