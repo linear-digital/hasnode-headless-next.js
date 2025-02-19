3937e6c7744b3783b12f41daefd6e5f65d866276
linear-digital <tamizuddin.tr@gmail.com>
2025-02-19 23:29:21 +0600

    update _document.tsx and pnpm-lock.yaml

--- packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_document.tsx
@@ -1,8 +1,22 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+declare module 'react' {
+	interface HTMLAttributes<T> {
+		amp?: string;
+	}
+}
+
+declare global {
+	namespace JSX {
+		interface IntrinsicElements {
+			'amp-auto-ads': any;
+		}
+	}
+}
+
 export default function Document() {
 	return (
-		<Html amp lang="en">
+		<Html lang="en">
 			<Head>
 				{/* Google AdSense Meta Tag */}
 				<meta name="google-adsense-account" content="ca-pub-4236044300590126" />
@@ -11,12 +25,11 @@ export default function Document() {
 				<script
 					async
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4236044300590126"
-					crossOrigin="anonymous"
 				></script>
 
 				{/* AMP Auto Ads Script */}
-				<script 
-					async 
+				<script
+					async
 					custom-element="amp-auto-ads"
 					src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
 				></script>
