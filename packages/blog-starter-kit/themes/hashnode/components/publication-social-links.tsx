df5e6ce4da7ec6e433768815a160321b5b81c3a7
linear-digital <tamizuddin.tr@gmail.com>
2025-02-19 18:25:36 +0600

    update post-page-navbar.tsx, publication-footer.tsx and publication-social-links.tsx

--- packages/blog-starter-kit/themes/hashnode/components/publication-social-links.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/publication-social-links.tsx
@@ -94,14 +94,6 @@ function PublicationSocialLinks(props: Props) {
                 </PublicationSocialLinkItem>
               );
             })}
-          <PublicationSocialLinkItem
-            key="rss"
-            href="rss.xml"
-            labelText="Open blog XML Feed, opens in new tab"
-            isSidebar={!!isSidebar}
-          >
-            <RssSVG className="h-5 w-5 fill-current" />
-          </PublicationSocialLinkItem>
         </div>
       ) : null}
     </>
